62b731105bfd31bd3034d0880641741abbefcc84
DimaUpGrade <dima2002bord@yandex.ru>
2023-03-22 22:17:41 +0300

    Lab 4 has been done.

--- lab4/README.md
+++ lab4/README.md
@@ -1,16 +1,6 @@
 # lab4
 
-A new Flutter project.
+Flutter project about lab 4 for mobile development.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Here I've created my page with description. photo, number, map coordinates
+and address of dormitory 18 of KubSAU.
--- lab4/lib/main.dart
+++ lab4/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dormitories of KubSAU',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Общежития КубГАУ'),
     );
@@ -30,15 +32,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // Текст названия общежития
+  String nameDorm = 'Общежитие 20';
 
-  //
+  // Текст адреса общежития
+  String addressDorm = 'Краснодар, ул. им. Калинина, 13к18';
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  // Текст описания на странице
+  String maintext = "Студенческий городок или так называемый кампус Кубанского ГАУ состоит из двадцати общежитий, в которых проживает более 8000 студентов, что составляет 96% от всех нуждающихся. Студенты первого курса обеспечены местами в общежитии полностью. В соответствии с Положением о студенческих общежитиях университета, при поселении между администрацией и студентами заключается договор найма жилого помещения. Воспитательная работа в общежитиях направлена на улучшение быта, соблюдение правил внутреннего распорядка, отсутствия асоциальных явлений в молодежной среде. Условия проживания в общежитиях университетского кампуса полностью отвечают санитарным нормам и требованиям: наличие оборудованных кухонь, душевых комнат, прачечных, читальных залов, комнат самоподготовки, помещений для заседаний студенческих советов и наглядной агитации. С целью улучшения условий быта студентов активно работает система студенческого самоуправления - студенческие советы организуют всю работу по самообслуживанию";
+
+  // Телефонный номер для кнопки
+  String phoneNumber = 'tel://+79181337228';
+
+  // Ссылка на общежитие 20 на Яндекс Карты
+  String roadLink = 'https://yandex.ru/maps/-/CCUSqEuFxD';
+
+  // Цвет кнопки лайка
+  MaterialColor likeIconColor = Colors.green;
+
+  // Проверка нажатой кнопки
+  bool checkButton = false;
+
+  // Количество лайков
+  int likesCnt = 16;
+
+
+  void LikeFunction() {
+    checkButton = !checkButton;
+    if (checkButton){
+      setState(() {
+        likeIconColor = Colors.red;
+      });
+      likesCnt++;
+    }
+    else {
+      setState(() {
+        likeIconColor = Colors.green;
+      });
+      likesCnt--;
+    }
   }
+
+  void PhoneFunction () {
+    launch(phoneNumber);
+  }
+
+  void RoadFunction () {
+    launch(roadLink);
+  }
+
+  void ShareFunction () {
+    Share.share(nameDorm);
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,25 +92,150 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: ListView (
+        children: [
+          Image.asset('images/dorm.jpg'),
+          Row(
+            children: [
+              SizedBox(
+                width: 300,
+                child: ListTile(
+                  title: Text(
+                    nameDorm,
+                  ),
+                  subtitle: Text(
+                    addressDorm,
+                  ),
+                )
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  foregroundColor: Colors.cyan,
+                ),
+                onPressed: LikeFunction,
+                child: Icon(
+                  Icons.favorite,
+                  color: likeIconColor,
+                ),
+              ),
+              Text(
+                  '$likesCnt'
+              )
+            ],
+          ),
+        const Divider (
+          color: Colors.transparent,
+          height: 15,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          Row(
+            children: [
+              Expanded(
+                  child: Column (
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.green,
+                        ),
+                        onPressed: PhoneFunction,
+                        child: Column(
+                          children: const [
+                            Icon(
+                              Icons.call,
+                              color: Colors.green,
+                            ),
+                            Divider(
+                              color: Colors.transparent,
+                              height: 10,
+                            ),
+                            Text(
+                                'ПОЗВОНИТЬ',
+                              style: TextStyle(color: Colors.green),
+                            )
+                          ]
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+              Expanded(
+                  child: Column (
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.green,
+                        ),
+                        onPressed: RoadFunction,
+                        child: Column(
+                            children: const [
+                              Icon(
+                                Icons.navigation,
+                                color: Colors.green,
+                              ),
+                              Divider(
+                                color: Colors.transparent,
+                                height: 10,
+                              ),
+                              Text(
+                                'МАРШРУТ',
+                                style: TextStyle(color: Colors.green),
+                              )
+                            ]
+                        ),
+                      ),
+                    ],
+                  )
+              ),
+              Expanded(
+                  child: Column (
+                    children: [
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.green,
+                        ),
+                        onPressed: ShareFunction,
+                        child: Column(
+                            children: const [
+                              Icon(
+                                Icons.share,
+                                color: Colors.green,
+                              ),
+                              Divider(
+                                color: Colors.transparent,
+                                height: 10,
+                              ),
+                              Text(
+                                'ПОДЕЛИТЬСЯ',
+                                style: TextStyle(color: Colors.green),
+                              )
+                            ]
+                        ),
+                      ),
+                    ],
+                  )
+              )
+            ],
+          ),
+          const Divider(
+            color: Colors.transparent,
+            height: 20,
+          ),
+          Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 340,
+                child: Text(
+                    maintext,
+                ),
+              )
+            ],
+          )
+        ],
+      )
     );
   }
 }
